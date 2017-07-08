4ea761b14e49a9f9b917e327010fa16188349581
hahaqin <qb560@vip.qq.com>
2017-07-08 18:17:31 +0800

    进入客户列表Servlet编码

--- src/main/java/org/stupid4j/web/controller/CustomerServlet.java
+++ src/main/java/org/stupid4j/web/controller/CustomerServlet.java
@@ -2,12 +2,16 @@ package org.stupid4j.web.controller;
 
 
 
+import org.stupid4j.web.model.Customer;
+import org.stupid4j.web.service.CustomerService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * 进入客户列表页面
@@ -15,6 +19,12 @@ import java.io.IOException;
  */
 @WebServlet("/customer")
 public class CustomerServlet extends HttpServlet {
+
+    private CustomerService customerService;
+
+    public void init() throws ServletException{
+        customerService = new CustomerService();
+    }
 
     /**
      * 进入 客户列表 页面
@@ -25,6 +35,8 @@ public class CustomerServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //TODO
+        List<Customer> customerList = customerService.getCustomerList();
+        req.setAttribute("customerList", customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req, resp);
     }
 }
